572d15f84a146b14f03b3322b357b4120fe79b1f
MWillian <Matheuswno02@gmail.com>
2026-03-06 12:34:10 -0300

    Adicionando a entidade usuário no projeto.

---
+++ CashFlow/src/CashFlow.Domain/Entities/User.cs
@@ -0,0 +1,14 @@
+﻿using CashFlow.Domain.Enums;
+
+namespace CashFlow.Domain.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public Guid UserIdentifier { get; set; }
+        public string Role { get; set; } = string.Empty;
+    }
+}
